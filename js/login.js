a5c5eb0cbd1af8a255cf104287e3ece52ed1f02e
hw <sagekhw@gmail.com>
2021-03-19 10:50:04 +0900

    1

--- js/login.js
+++ js/login.js
@@ -9,6 +9,7 @@ function getParameter(str_param) {
 
 function init() {
     alert("hello");
+    alert("@ : "+getParameter("name"));
     if (getParameter("name") != null) {
         alert(getParameter("name"));
     }
@@ -17,13 +18,7 @@ function init() {
     }
 }
 
-$("#loginBtn").click(function(){
-    location.href="https://accounts.google.com/o/oauth2/auth?client_id="+
-    "615811997217-prn8q5o8juolfn38efjdot0vb5gni4kg.apps.googleusercontent.com"+
-    "&redirect_uri="+
-    "http://localhost:8080/redirect" +
-    "&response_type=code&scope=email profile openid&approval_prompt=force&access_type=offline";
-  });
+
   
 function move_signup_page() {
   window.location.replace("http://localhost/moze_shop/html/signup_nav.html"); 
@@ -34,3 +29,10 @@ window.onload = function() {
     // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
     init();
 }
+$("#loginBtn").click(function(){
+    location.href="https://accounts.google.com/o/oauth2/auth?client_id="+
+    "615811997217-prn8q5o8juolfn38efjdot0vb5gni4kg.apps.googleusercontent.com"+
+    "&redirect_uri="+
+    "http://localhost:8080/redirect" +
+    "&response_type=code&scope=email profile openid&approval_prompt=force&access_type=offline";
+  });
