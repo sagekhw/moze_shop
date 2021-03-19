c219319fb32b11ef489b1f76376e6f53bc79ea55
hw <sagekhw@gmail.com>
2021-03-19 14:16:36 +0900

    1

--- js/login.js
+++ js/login.js
@@ -21,17 +21,52 @@ function init() {
 function move_signup_page() {
   window.location.replace("http://localhost/moze_shop/html/signup_nav.html"); 
 }
-
 function login(){
+    var user = $("#user").val();
+    var password = $("#password").val();
+
+    if(user == "" && password == ""){
+        alert("정보를 입력해주세요");
+        return 0;
+    }
+    var formData = { 
+        userid : user,
+        password : password,
+    };
+    var json_formData = JSON.stringify(formData);
+    alert(json_formData);
+
+    $.ajax({
+        url:'http://localhost:8080/login',
+        type:'POST',
+        async: false, 
+        contentType:'application/json',
+        data:json_formData,
+        success: function(){
+            //정상 요청, 응답 시 처리 작업
+            alert("success");
+           
+        },
+        error : function(xhr,status,error) {
+            alert("error"+status+" / "+error);
+            //오류 발생 시 처리
+        },
+        complete:function(data,textStatus) {
+            //작업 완료 후 처리
+            // alert("complete");
+        }
+    });
+}
+
+function google_login(){
     location.href="https://accounts.google.com/o/oauth2/auth?client_id="+
                 "615811997217-prn8q5o8juolfn38efjdot0vb5gni4kg.apps.googleusercontent.com"+
                 "&redirect_uri="+
                 "http://localhost:8080/redirect_google_login"+
                 "&response_type=code&scope=email profile openid&approval_prompt=force&access_type=offline";
-    
 }
 
 window.onload = function() {
     // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
-    init();
+    // init();
 }
