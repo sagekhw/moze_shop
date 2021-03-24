e06b4d835f3ea407f5ccf1e001cf86c480528129
hw <sagekhw@gmail.com>
2021-03-24 23:04:37 +0900

    8

--- html/js/home.js
+++ html/js/home.js
@@ -1,7 +1,32 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
 window.onload = function() {
     // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
 
     var jwt = getCookie("jwt");
+    alert(jwt);
     if(jwt != ""){
         var str = $("#login_text").val();
         alert(str);
