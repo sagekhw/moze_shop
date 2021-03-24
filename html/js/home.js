327f540b17df65feb30a2f40290e0eb6fc466141
hw <sagekhw@gmail.com>
2021-03-24 23:08:28 +0900

    9

--- html/js/home.js
+++ html/js/home.js
@@ -26,9 +26,9 @@ window.onload = function() {
     // 페이지가 로딩된 후 실행해야 되는 코드를 추가한다.
 
     var jwt = getCookie("jwt");
-    alert(jwt);
+    
     if(jwt != ""){
-        var str = $("#login_text").val();
-        alert(str);
+        alert(jwt);
+        var str = $("#login_text").text("로그아웃");
     }
 }
